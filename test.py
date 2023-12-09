733b0fc036e43366d1e085f67c3ac8bae566fae5
Lucas Hiroshi Horinouchi <lucas.hh@hotmail.com>
2023-12-09 15:34:23 +0200

    this is a new commit

---
+++ test.py
@@ -0,0 +1,18 @@
+# BEGIN: FILEPATH: /C:/Users/lucas/Documents/GitHub/wrapup-day2/test.txt
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Error: Division by zero is not allowed."
+
+# END: FILEPATH: /C:/Users/lucas/Documents/GitHub/wrapup-day2/test.txt
